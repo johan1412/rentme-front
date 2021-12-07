64999b82152456594c30c455a9cae0d41dd93c62
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2021-12-08 00:50:53 +0100

    Project structure

---
+++ src/components/forms/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
---
+++ src/components/pages/authentification/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link to="/">HOME</router-link><br />
+
+    <h1>Login</h1>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from "../../forms/LoginForm.vue";
+export default {
+  name: "Login",
+  components: {
+    LoginForm,
+  },
+};
+</script>
