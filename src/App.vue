ec7d39122551ec3af88ac936d86309dbbaef4a2f
johan <johan.travert@aliceadsl.fr>
2021-12-13 17:07:35 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-   <router-view />
+    <div id="app">
+        <Header />
+        <Navbar />
+        <router-view />
+    </div>
 </template>
 
 <script>
+import Navbar from './components/layouts/Navbar.vue';
+import Header from './components/layouts/Header.vue';
 
 export default {
   name: "App",
   components: {
-    
+    Navbar,
+    Header,
   },
 };
 </script>
 
 <style>
-
+    body {
+        margin: 0px;
+    }
+    #app {
+        background-color: #F0F0F0;
+        height: 100vh;
+        margin: 0px;
+    }
 </style>
