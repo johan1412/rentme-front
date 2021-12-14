3bf58ed232a81b9877c690812621a94f043e55c5
Abdellatif CHALAL <abdellatifchalal44@gmail.com>
2021-12-14 20:43:25 +0100

    add auth

---
+++ .history/src/components/Home_20211214193817.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+        <nav-bar />
+<div class="containert">
+
+</div>
+</div>
+</template>
+<script>
+import NavBar from './layouts/NavBar.vue';
+export default {
+  name: "Home",
+  components:{
+    NavBar
+  }
+};
+</script>
---
+++ .history/src/components/layouts/Home_20211214194439.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+        <nav-bar />
+<div class="containert m-auto">
+  <h1 class="text-center">Je suis le client</h1>
+</div>
+</div>
+</template>
+<script>
+import NavBar from './layouts/NavBar.vue';
+export default {
+  name: "Home",
+  components:{
+    NavBar
+  }
+};
+</script>
