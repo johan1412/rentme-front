4dcb1ea75bef4221ae3d4955cb5a06daa87daf1c
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-02-15 17:47:56 +0100

    add image to product

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -7,44 +7,51 @@ const headers = {
  }
 
 const login = async data => {
-  return await axios.post("authentication_token", data);
+  return await axios.post("https://localhost:8443/authentication_token", data);
 };
 
 const register = async data => {
-  return await axios.post("users", data);
+  return await axios.post("https://localhost:8443/users", data);
 };
 
 const getUser = async (id) => {
-  return await axios.get(`users/${id}`, {headers});
+  return await axios.get(`https://localhost:8443/users/${id}`, {});
 };
 
 const parentCategories = async () => {
-  return await axios.get("categories", {headers});
+  return await axios.get("https://localhost:8443/categories", {headers});
 };
 
 const postCategory = async (data) => {
-  return await axios.post("categories", data, {headers});
+  return await axios.post("https://localhost:8443/categories", data, {headers});
 };
 
 const getProducts = async () => {
-  return await axios.get("products", {headers});
+  return await axios.get("https://localhost:8443/products", {headers});
 };
 
 const getProductsNotValid = async () => {
-  return await axios.get("products/products-not-valid", {headers});
+  return await axios.get("https://localhost:8443/products/products-not-valid", {headers});
 };
 
 const updateProduct = async (id,data) => {
-  return await axios.patch(`products/${id}`,data, {headers});
+  return await axios.patch(`https://localhost:8443/products/${id}`,data, {headers});
 };
 
 
 const deleteProduct = async (id) => {
-  return await axios.delete(`products/${id}`, {headers});
+  return await axios.delete(`https://localhost:8443/products/${id}`, {headers});
 };
 
 const postProduct = async (data) => {
-  return await axios.post("products", data, {headers});
+  return await axios.post("https://localhost:8443/products", data, {headers});
+};
+
+const postImage = async (data) => {
+  return await axios.post("https://localhost:8443/media_objects", data, {
+    'Content-type': 'multipart/form-data',
+    'Authorization': 'Bearer '+localStorage.getItem('token')
+   });
 };
 
 export default {
@@ -57,5 +64,6 @@ export default {
   updateProduct,
   deleteProduct,
   getProductsNotValid,
-  postProduct
+  postProduct,
+  postImage
 };
