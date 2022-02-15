ff9a6b5a274152d662af56a0ed8b123f746448f9
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-02-15 23:19:55 +0100

    messages front v0.9

--- src/components/pages/common/MessagesPage.vue
+++ src/components/pages/common/MessagesPage.vue
@@ -1,55 +1,69 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="list-conversations col-3">
-        <div v-for="conversationsByProduct, productId of conversations/*.data*/" :key="conversationsByProduct.id">
-            <div v-for="conversation, otherUser in conversationsByProduct" :key="conversation.id">
-              <h4>{{ otherUser }}</h4>
-              <h4>Produit : {{productId}}</h4>
-            </div>
-        </div>
-      </div>
-      <div class="conversation-content col-9">
-        <Conversation :messages="conversation" />
+  <div class="accordion" role="tablist">
+    <div
+      v-for="(conversationsByProduct, productId) of conversations.data"
+      :key="conversationsByProduct.id"
+    >
+      <div
+        v-for="(conversation, otherUser) in conversationsByProduct"
+        :key="conversation.id"
+      >
+       
+
+
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-1 variant="info">
+               <h3>productID: {{ productId }}</h3>
+               <h3>OtherUser: {{ otherUser }}</h3>
+              </b-button
+            >
+          </b-card-header>
+                <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+
+            <b-card-body>
+              <b-card-text
+                >
+                        <Conversation :messages="conversation" />
+                        <input type="text" />
+                        <button>Envoyer</button>
+
+                </b-card-text
+              >
+              
+            </b-card-body>
+          </b-collapse>
+        </b-card>
       </div>
     </div>
   </div>
+
+   
 </template>
 <script>
 import Conversation from "../../layout/Conversation.vue";
 import MessagesService from "../../../services/MessagesService";
-
 export default {
   components: {
     Conversation,
-    },
+  },
   name: "MessagesPage",
   data() {
     return {
       conversations: [],
-    }
+      conversationsWithNames: [],
+    };
   },
   async mounted() {
-    /////////////////////////////////////// POUR TESTER DEBUT
-    let conversations = [];
-    let conversationsI = [];
-    for(let i=0; i < 3; i++) {
-      for(let j=0; j < 4; j++) {
-        conversationsI.push({ id: 1, text: 'message de test pour vérifier si tout fonctionne bien, merci bien bonne journée', renderId: 1, receiverId: 3, createdAt: '14/02/2022', productId: 5 });
-      }
-      conversations.push(conversationsI);
-    }
-    this.conversations = conversations;
-    /////////////////////////////////////// FIN
-    if (this.$store.getters.user) {
-      this.conversations = await MessagesService.getConversations(this.$store.getters.user["@id"].split("/")[2])
+    this.conversations = await MessagesService.getConversations(
+      this.$store.getters.user["@id"].split("/")[2]
+    );
+  },
+  methods: {
+    conversationToggle(id) {
+      document.getElementById(id).classList.toggle('visible')
     }
   }
 };
 </script>
 
-<style scoped>
-.list-conversations {
-  border-right: 1px solid #666666;
-}
-</style>
