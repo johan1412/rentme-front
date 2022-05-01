e0fa1fdde4497d0cd42e7b22835ede0b9d19fa18
t.johan14 <johan.travert@aliceadsl.fr>
2022-05-01 16:34:24 +0200

    user can edit his account

--- src/components/layout/BaseLayout.vue
+++ src/components/layout/BaseLayout.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="app">
     <NavBar/>
-    <div class="alert alert-success text-center mt-5 p-3" v-if="messageNotification">
+    <div class="alert-message alert alert-success text-center mt-5 p-3" v-if="messageNotification">
       {{ messageNotification }}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true" class="ml-5">&times;</span>
+      </button>
     </div>
     <slot></slot>
     <Footer />
@@ -61,5 +64,11 @@ export default {
 .app {
   background-color: #f0f0f0;
   font-family: 'Roboto';
+}
+
+.alert-message {
+  width: fit-content;
+  min-width: 150px;
+  margin: auto;
 }
 </style>
