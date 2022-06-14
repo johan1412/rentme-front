14310f247035e11976a6ed8314b8258274d33efb
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-06-14 22:37:47 +0200

    make productList compenent general

--- src/components/pages/renter/RenterProductsPage.vue
+++ src/components/pages/renter/RenterProductsPage.vue
@@ -2,18 +2,31 @@
   <div class="container">
     <h1 class="h3 mt-5">Mes annonces</h1>
     <hr>
-    <Products-list />
+    <Products-list :products="products" />
   </div>
 </template>
 <script>
 import ProductsList from "../../layout/ProductsList.vue";
+import authService from "@/services/AuthService";
 
 
 export default {
   components: {
     ProductsList,
   },
-  name: "RenterProductsPage"
+  data() {
+    return {
+      products: [],
+    }
+  },
+  name: "RenterProductsPage",
+  methods: {
+
+  },
+  async created() {
+    const user = await authService.getUser(this.$store.getters.user["@id"].split("/")[2])
+    this.products = user.data.products
+  }
 
 };
 </script>
