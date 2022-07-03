eb4ff8324683a88ea9d009a2c873ef7ef001533b
t.johan14 <johan.travert@aliceadsl.fr>
2022-07-03 15:13:28 +0200

    add delete option for products, categories, regions + some edition

--- src/services/RegionService.js
+++ src/services/RegionService.js
@@ -3,19 +3,43 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.VUE_APP_URL
 const headers = {
   'Content-type': 'application/json',
- }
+}
 
- const postRegion = async (data) => {
-     axios.defaults.baseURL = process.env.VUE_APP_URL
-    return await axios.post("regions", data, {headers});
-  };
-  
-  const getRegions = async () => {
-      axios.defaults.baseURL = process.env.VUE_APP_URL
-    return await axios.get('regions', {headers});
-  };
+const postRegion = async (data, token) => {
+  axios.defaults.baseURL = process.env.VUE_APP_URL
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'Bearer '+token
+  }
+  return await axios.post("regions", data, {headers});
+};
+
+const getRegions = async () => {
+  axios.defaults.baseURL = process.env.VUE_APP_URL
+  return await axios.get('regions', {headers});
+};
+
+const updateRegion = async (id, data, token) => {
+  axios.defaults.baseURL = process.env.VUE_APP_URL
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'Bearer '+token
+  }
+  return await axios.put(`regions/${id}`, data, {headers});
+};
+
+const deleteRegion = async (id, token) => {
+  axios.defaults.baseURL = process.env.VUE_APP_URL
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'Bearer '+token
+  }
+  return await axios.delete(`regions/${id}`, {headers});
+};
 
 export default {
   postRegion,
   getRegions,
+  updateRegion,
+  deleteRegion,
 };
