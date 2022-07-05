2004e265947893e344785ffc47ad86a403a35f21
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-07-06 01:12:28 +0200

    add middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+
 let app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.use(history()).listen(port, () => {
     console.log('Listening on port ' + port)
 });
 
