688be7416cde9a74b170326ea4876c131260fdaa
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-07-06 09:17:18 +0000

    Revert "add middleware"
    This reverts commit 2004e265947893e344785ffc47ad86a403a35f21

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
-
 let app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 8080;
-app.use(history()).listen(port, () => {
+app.listen(port, () => {
     console.log('Listening on port ' + port)
 });
 
